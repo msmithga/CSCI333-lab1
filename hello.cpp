34f884c84693717c182201b51c31bff9b8e1406b
Mary Smith-Gagnon <msmithga@unca.edu>
2012-09-05 09:48:46 -0400

    added first euler problem

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,21 @@
 using std::cout;
 using std::endl;
 
+int euler(int n);
+
 int main() {
   cout << "hello world" << endl;
+  cout << "result: " << euler(1000) << endl;
   return 0;
+}
+
+int euler(int n) {
+  int sum = 0;
+
+  for(int i = 0; i < n; ++i) {
+    if(i%3 == 0 || i%5 == 0) {
+      sum += i;
+    }
+  }
+  return sum;
 }
